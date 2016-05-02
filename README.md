898a3561fa502b2f8537103f1c70dc63680c53ed
Dmitry Patsura <zaets28rus@gmail.com>
2016-05-02 13:27:20 +0300

    PHP 7.0.6 Release :3

--- README.md
+++ README.md
@@ -7,13 +7,15 @@ Docker Images for LowL.CI
 
 | OS                         | Language | Version        |
 |----------------------------|----------|----------------|
-| Ubuntu 16.04 Xenial Xerus  | PHP      | 7.0.5 (latest) |
+| Ubuntu 16.04 Xenial Xerus  | PHP      | 7.0.6 (latest) |
+|                            |          | 7.0.5          |
 |                            |          | 7.0.4          |
 |                            |          | 7.0.3          |
 |                            |          | 7.0.2          |
 |                            |          | 7.0.1          |
 |                            |          | 7.0.0          |
-| Ubuntu 15.10 Wily Werewolf | PHP      | 7.0.5 (latest) |
+| Ubuntu 15.10 Wily Werewolf | PHP      | 7.0.6 (latest) |
+|                            |          | 7.0.5          |
 |                            |          | 7.0.4          |
 |                            |          | 7.0.3          |
 |                            |          | 7.0.2          |
--- php/ubuntu/16.04-xenial/build.sh
+++ php/ubuntu/16.04-xenial/build.sh
@@ -1,34 +1,39 @@
-# 7.0.5 / Latest
+# 7.0.6 / Latest
+cd 7.0.6
+docker build -t lowlci/php-ubuntu-1604:latest - < Dockerfile
+docker push lowlci/php-ubuntu-1604:latest
+docker build -t lowlci/php-ubuntu-1604:7.0.6 - < Dockerfile
+docker push lowlci/php-ubuntu-1604:7.0.6
+
+# 7.0.5
 cd 7.0.5
-docker build -t lowlci/php7-ubuntu-1604:latest - < Dockerfile
-docker push lowlci/php7-ubuntu-1604:latest
-docker build -t lowlci/php7-ubuntu-1604:7.0.5 - < Dockerfile
-docker push lowlci/php7-ubuntu-1604:7.0.5
+docker build -t lowlci/php-ubuntu-1604:7.0.5 - < Dockerfile
+docker push lowlci/php-ubuntu-1604:7.0.5
 
 cd ..
 
 # 7.0.4
 cd 7.0.4
-docker build -t lowlci/php7-ubuntu-1604:7.0.4 - < Dockerfile
-docker push lowlci/php7-ubuntu-1604:7.0.4
+docker build -t lowlci/php-ubuntu-1604:7.0.4 - < Dockerfile
+docker push lowlci/php-ubuntu-1604:7.0.4
 
 cd ..
 
 # 7.0.3
 cd 7.0.3
-docker build -t lowlci/php7-ubuntu-1604:7.0.3 - < Dockerfile
-docker push lowlci/php7-ubuntu-1604:7.0.3
+docker build -t lowlci/php-ubuntu-1604:7.0.3 - < Dockerfile
+docker push lowlci/php-ubuntu-1604:7.0.3
 
 cd ..
 
 # 7.0.2
 cd 7.0.2
-docker build -t lowlci/php7-ubuntu-1604:7.0.2 - < Dockerfile
-docker push lowlci/php7-ubuntu-1604:7.0.2
+docker build -t lowlci/php-ubuntu-1604:7.0.2 - < Dockerfile
+docker push lowlci/php-ubuntu-1604:7.0.2
 
 cd ..
 
 # 7.0.1
 cd 7.0.1
-docker build -t lowlci/php7-ubuntu-1604:7.0.1 - < Dockerfile
-docker push lowlci/php7-ubuntu-1604:7.0.1
+docker build -t lowlci/php-ubuntu-1604:7.0.1 - < Dockerfile
+docker push lowlci/php-ubuntu-1604:7.0.1
